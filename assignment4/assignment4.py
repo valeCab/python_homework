b17a021f9762a9c4aa4abc8cf93972f742340b25
Valeria <valeria.cabrerapacheco18@gmail.com>
2026-07-26 22:42:57 -0400

    Complete Assignment 4

---
+++ assignment4/assignment4.py
@@ -0,0 +1,108 @@
+# Task1: Intro to Pandas
+import pandas as pd 
+
+#1. 
+data = {
+    'Name': ['Alice', 'Bob', 'Charlie' ],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+}
+
+task1_data_frame = pd.DataFrame(data)
+print(task1_data_frame)
+
+#2.
+task1_with_salary = task1_data_frame.copy()
+task1_with_salary['Salary'] = [70000, 80000, 90000]
+
+print(task1_with_salary)
+
+#3. 
+task1_older = task1_with_salary.copy()
+task1_older['Age'] = task1_older['Age'] + 1 
+
+print(task1_older)
+
+#4.
+task1_older.to_csv('employees.csv', index=False)
+task1_older.info()
+
+#Task2: Loading Data
+
+import json 
+
+#1.
+task2_employees = pd.read_csv("employees.csv")
+print(task2_employees)
+
+#2. 
+additional_employees = [
+    {'Name': 'Eve', 'Age': 28, 'City': 'Miami', 'Salary': 60000},
+    {'Name': 'Frank', 'Age': 40, 'City': 'Seattle', 'Salary': 95000},
+]
+with open("additional_employees.json", "w") as f:
+    json.dump(additional_employees, f)
+
+json_employees = pd.read_json("additional_employees.json")
+print(json_employees)
+
+#3. 
+
+more_employees = pd.concat([task2_employees, json_employees], ignore_index=True)
+print(more_employees)
+
+#Task3: Data Inspection 
+
+#1. 
+first_three = more_employees.head(3)
+print(first_three)
+
+#2. 
+last_two = more_employees.tail(2)
+print(last_two)
+
+#3. 
+employee_shape = more_employees.shape
+print(employee_shape)
+
+#4.
+more_employees.info()
+
+#Task4: Data Cleaning
+
+import numpy as np
+
+#1. 
+dirty_data = pd.read_csv("dirty_data.csv")
+print(dirty_data)
+
+clean_data = dirty_data.copy()
+
+#2. 
+clean_data = clean_data.drop_duplicates()
+print(clean_data)
+
+#3. 
+clean_data["Age"] = pd.to_numeric(clean_data["Age"], errors="coerce")
+print(clean_data)
+
+#4.
+clean_data["Salary"] = clean_data["Salary"].replace(["unknown", "n/a"], np.nan)
+clean_data["Salary"] = pd.to_numeric(clean_data["Salary"], errors="coerce")
+print(clean_data)
+
+#5. 
+clean_data["Age"] = clean_data["Age"].fillna(clean_data["Age"].mean())
+clean_data["Salary"] = clean_data["Salary"].fillna(clean_data["Salary"].median())
+print(clean_data)
+
+#6.
+clean_data["Hire Date"] = pd.to_datetime(
+    clean_data["Hire Date"], errors="coerce"
+)
+print(clean_data)
+
+#7.
+clean_data["Name"] = clean_data["Name"].astype(str).str.strip().str.upper()
+clean_data["Department"] = clean_data["Department"].astype(str).str.strip().str.upper()
+print(clean_data)
